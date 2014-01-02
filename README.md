3a76204f523f9349cea70dba641313c39a63a5d3
Martin Thoma <info@martin-thoma.de>
2014-01-02 20:25:32 +0100

    added caption tag

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 jekyll-caption-tag
 ==================
 
-Add a caption tag to Jekyll
+A Liquid tag for Jekyll sites that allows easy creation of captioned
+images like they are in WordPress.
+
+How To Install
+===============
+1. Copy `caption_tag.rb` into `<your-jekyll-site>/_plugins`.
+2. Add the styles from `style.css` to your stylesheets.
+
+How To use
+==========
+Place a `caption` tag in your content file, e.g.:
+```
+{% caption align="aligncenter" width="512" height="233" alt="The order of points is important for the definition of a polygon" text="[A, B, C, D, E, F, G] != [A, B, C, D, F, E, G]" url="../images/2013/11/polygon-order.png" %}
+```
+
+Changelog
+=========
+Version 1.0, 2014-01-02: Initial commit.
+
+License
+=======
+This code is licensed under MIT License. 
+
+What does this mean?
+
+* You may use it if you like
+* Don't blame me when thinks don't work (though you can file an issue)
+* Please don't remove the link in the comments of `caption_tag.rb`.
+  This might help others to find this plugin
---
+++ caption_tag.rb
@@ -0,0 +1,53 @@
+# A Liquid tag for Jekyll sites that allows easy creation of captioned
+# images like they are in WordPress.
+#
+# Author: Martin Thoma (info@martin-thoma.de)
+# Source: https://github.com/MartinThoma/jekyll-caption-tag
+# Version: 1.0
+#
+# Example usage:
+#   {% caption align="aligncenter" width="500" alt="WER calculation" text="WER calculation" url="/images/2013/11/WER-calculation.png" %}
+#
+# Plugin replaces the template above with:
+#    <div style="width: 510px" class="wp-caption aligncenter">
+#        <a href="/images/2013/11/WER-calculation.png">
+#            <img src="/images/2013/11/WER-calculation.png" alt="WER calculation" width="500" height="494" class="size-full">
+#        </a>
+#        <p class="wp-caption-text">WER calculation</p>
+#    </div>
+
+require 'csv' 
+
+module Jekyll
+  class CaptionTag < Liquid::Tag
+
+    def initialize(tag_name, text, tokens)
+      super
+      @text = text
+      @tokens = tokens
+    end
+
+    def parse_attrs(input)
+      options = { col_sep: '=', row_sep: ' ', quote_char: '"' }
+      csv = CSV.new input, options
+
+      csv.each_with_object({}) do |row, attrs|
+        attr, value = row
+        value ||= true
+        attrs[attr] = value
+      end
+    end
+
+    def render(context)
+        @hash = parse_attrs(@text)
+        "<div style=\"width: #{@hash['width']}px\" class=\"wp-caption #{@hash['align']}\">" +
+        "<a href=\"" + context.registers[:site].config['baseurl'] + "/images/#{@hash['url']}\">" +
+            "<img src=\"" + context.registers[:site].config['baseurl'] + "/images/#{@hash['url']}\" alt=\"#{@hash['text']}\" width=\"#{@hash['width']}\" height=\"#{@hash['height']}\" class=\"#{@hash['class']}\">" +
+        "</a>" +
+        "<p class=\"wp-caption-text\">#{@hash['text']}</p>" +
+        "</div>"
+    end
+  end
+end
+
+Liquid::Template.register_tag('caption', Jekyll::CaptionTag)
